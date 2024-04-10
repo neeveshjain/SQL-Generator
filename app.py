58d86a586ffd14d6e4e8f13c0cace99ff407af2b
Neevesh Jain <neeveshjain27@gmail.com>
2024-04-10 11:29:24 +0530

    started connecting DB

--- app.py
+++ app.py
@@ -1,78 +1,28 @@
 import streamlit as st
 import google.generativeai as genai
+import simple
 
 GOOGLE_API_KEY ="AIzaSyCdGCm8Ge6rEARQ6HnG7qunp1vOJzltlnc"
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel( "gemini-pro")
 
-
 def main():
-    st.set_page_config(page_title="SQL Query Generator", page_icon=":robot:")
-    st.markdown(
-        """
-        <div style = "text-align:center;>"
-        <h1>   SQL Query Generator bot🤖</h1>
-        <h3>   An A.I tool to generate SQL queries </h3>
-        <p>    This tool is designed to help you generate and explain SQL queries for your data. </p>
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
+    st.sidebar.title("Nav-bar")
+    app_mode = st.sidebar.selectbox("Choose a page", ["Home","simple", "advance"])
 
-    text_input = st.text_area("Enter your SQL query here, in plain english:")
+    if app_mode == "Home":
+        st.title("Choose one option")
+        st.write("simple: for simple sql generator")
+        st.write("advance: for advance sql generator")
+        
 
-    submit = st.button("Generate SQL Query")
+    elif app_mode == "simple":
+        # Add your content for the Home page here
+        simple.main()
 
-    if submit:
-        with st.spinner("Generating SQL Query..."):
-            template = """Create a SQL query snippet using the below txt:
-            ```
-            {text_input}
-            ```
-            I just want a SQL query
-            """
+    elif app_mode == "advance":
+        st.title("here I present advance sql genrator")
+        # Add your content for the About page here
 
-            formatted_template = template.format(text_input=text_input)
-#            st.write(formatted_template)
-            response = model.generate_content(formatted_template)
-            sql_query = response.text
-
-            sql_query = sql_query.strip().lstrip("```sql").rstrip("```")
-            st.write(sql_query)
-
-            expected_output = """
-            What would be the expected response of this SQL query snippet:
-            ```
-            {sql_query}
-            ```
-            provide sample tabular responses with no explaintation needed
-            """
-            
-            expexted_output_format = expected_output.format (sql_query=sql_query)
-            eoutput = model.generate_content(expexted_output_format)
-            eoutput = eoutput.text
-            st.write(eoutput)
-
-            explanation = """
-            What would be the expected response of this SQL query snippet:
-            ```
-            {sql_query}
-            ```
-            Please provide with step by step explanation:
-            """
-
-            explanation_format = explanation.format(sql_query=sql_query)
-            explanation = model.generate_content(explanation_format)
-            explanation = explanation.text
-            
-            with st.container():
-                st.write("\n\n")
-                st.success("SQL Query Generated Successfully")
-                st.code(sql_query,language="sql")
-                st.success("Expected Output of this SQL Query:")
-                st.markdown(eoutput)
-                st.write("\n\n")
-                st.success("Explanation:")
-                st.markdown(explanation,unsafe_allow_html=True)
-                
-main()
+if __name__ == "__main__":
+    main()
---
+++ simple.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import google.generativeai as genai
+
+GOOGLE_API_KEY ="AIzaSyCdGCm8Ge6rEARQ6HnG7qunp1vOJzltlnc"
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel( "gemini-pro")
+
+def main():
+    #st.set_page_config(page_title="SQL Query Generator", page_icon=":robot:")
+    #st.title("Simple text to SQL generator", anchor='center')
+    st.markdown(
+        """
+        <div style = "text-align:center;">
+        <h1 style="font-size: 30px;">SQL Query Generator bot🤖</h1>
+        <h3 style="font-size: 20px;">An A.I tool to generate SQL queries</h3>
+        <p style="font-size: 16px;">This tool is designed to help you generate and explain SQL queries for your data.</p>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    text_input = st.text_area("Enter your SQL query here, in plain english:")
+
+    submit = st.button("Generate SQL Query")
+
+    if submit:
+        with st.spinner("Generating SQL Query..."):
+            template = """Create a SQL query snippet using the below txt:
+            ```
+            {text_input}
+            ```
+            I just want a SQL query
+            """
+
+            formatted_template = template.format(text_input=text_input)
+#            st.write(formatted_template)
+            response = model.generate_content(formatted_template)
+            sql_query = response.text
+
+            sql_query = sql_query.strip().lstrip("```sql").rstrip("```")
+            st.write(sql_query)
+
+            expected_output = """
+            What would be the expected response of this SQL query snippet:
+            ```
+            {sql_query}
+            ```
+            provide sample tabular responses with no explaintation needed
+            """
+            
+            expexted_output_format = expected_output.format (sql_query=sql_query)
+            eoutput = model.generate_content(expexted_output_format)
+            eoutput = eoutput.text
+            st.write(eoutput)
+
+            explanation = """
+            What would be the expected response of this SQL query snippet:
+            ```
+            {sql_query}
+            ```
+            Please provide with step by step explanation:
+            """
+
+            explanation_format = explanation.format(sql_query=sql_query)
+            explanation = model.generate_content(explanation_format)
+            explanation = explanation.text
+            
+            with st.container():
+                st.write("\n\n")
+                st.success("SQL Query Generated Successfully")
+                st.code(sql_query,language="sql")
+                st.success("Expected Output of this SQL Query:")
+                st.markdown(eoutput)
+                st.write("\n\n")
+                st.success("Explanation:")
+                st.markdown(explanation,unsafe_allow_html=True)
+                st.write("\n\n")
+                st.info("This tool is powered by Google's Generative AI model")
+                st.write("\n\n")
+
+if __name__ == "__main__":
+    main()
